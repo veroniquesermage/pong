0f1ece5fe7f6512c7e33c5d598d93c73d4174813
Véronique Sermage <ransomstoddart@gmail.com>
2019-01-15 10:19:25 +0100

    test

--- app/src/main/java/fr/wcs/pong/MainActivity.java
+++ app/src/main/java/fr/wcs/pong/MainActivity.java
@@ -1,7 +1,9 @@
 package fr.wcs.pong;
 
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView ball = findViewById(R.id.ball);
+        ObjectAnimator animation = ObjectAnimator.ofInt(ball, "translationY", 1000);
+        animation.setDuration(2000);
+        animation.start();
     }
 }
